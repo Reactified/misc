b03fbec8df86d7cc3dfd2226b0b21236648a5089
Reactified <54720381+Reactified@users.noreply.github.com>
2021-05-09 15:09:58 +0000

    Security fixes
    Rewrote incineration function to **hopefully** make it bulletproof

--- lua/ccash-bank/backend.lua
+++ lua/ccash-bank/backend.lua
@@ -1,12 +1,12 @@
 -- CCash Exchange Backend
 local reserveAccount = "Reserve"
-local reservePassword = "testaccount"
-local reserveMaximum = 100
+local reservePassword = "RESERVE_PASSWORD_HERE"
+local reserveMaximum = 100000
 local commodity = "minecraft:coal"
 
 -- Backend Routine
 os.pullEvent = os.pullEventRaw
-local version = 1
+local version = 2
 local function backend()
 
     -- Load API
@@ -113,22 +113,44 @@ local function backend()
                 turtle.drop()
             end
         elseif cmd == "[DEPOSIT-CONFIRM]" then
+            local exRate = exchangeRate()
+
             local username = recv()
-            local depositValue = math.floor(depositAmount*exchangeRate())
+            local depositValue = math.floor(depositAmount*exRate)
             local reserveBalance
             repeat
                 reserveBalance = api.simple.balance(reserveAccount)
             until reserveBalance
-            if reserveBalance >= depositValue then
+
+            -- incinerate
+            local incinerationCount = 0
+            for i=1,16 do
+                if turtle.getItemCount(i) > 0 then
+                    turtle.select(i)
+                    local detail = turtle.getItemDetail()
+                    local count = turtle.getItemCount()
+                    if detail.name == commodity then
+                        incinerationCount = incinerationCount + (count * exRate)
+                    end
+                    if not turtle.dropDown() then
+                        -- if item was somehow stolen in this tiny window of time
+                        incinerationCount = incinerationCount - (count * exRate)
+                    end
+                end
+            end
+
+            if (depositValue ~= incinerationCount) then
+                print("!! INCINERATION ERROR !!")
+                print(incinerationCount)
+                print(depositValue)
+            end
+
+            if reserveBalance >= depositValue and (depositValue == incinerationCount) then
                 write("Attempting deposit... ")
                 local success = api.simple.send(reserveAccount, reservePassword, username, depositValue)
                 print(success)
                 if success then
                     send(true)
-                    for i=1,16 do
-                        turtle.select(i)
-                        turtle.dropDown()
-                    end
                     depositAmount = 0
                 else
                     send(false)
